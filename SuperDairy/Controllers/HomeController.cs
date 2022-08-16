a18f15d402dc835d9c8fd726077b63c10a25458f
Meghanath Nalawade <50794140+mr-compilerMS@users.noreply.github.com>
2022-08-16 10:48:43 +0530

    Added client side bill view

--- SuperDairy/Controllers/HomeController.cs
+++ SuperDairy/Controllers/HomeController.cs
@@ -38,6 +38,7 @@ namespace SuperDairy.Controllers
         {
             int userId = Int32.Parse(User.Claims.Where(u => u.Type == ClaimTypes.NameIdentifier).First()?.Value ?? "0");
             List<Bill> bills = Bill.GetBills(userId, Common.ConnectionString);
+            bills.RemoveAll(e => e.TotalAmount == 0);
             ViewData["Bills"]= bills;
             return View();
         }
@@ -46,6 +47,21 @@ namespace SuperDairy.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public ActionResult ShowBill(Guid id)
+        {
+            Bill bill = new Bill(id, Common.ConnectionString);
+            List<MilkInventory> list=MilkInventory.GetInventoryList(bill.UserId, bill.StartDate,bill.EndDate, Common.ConnectionString);
+            ViewData["InventoryList"] = list;
+            var totalMilk= list.Sum(e => e.Quantity);
+            ViewData["TotalMilk"] = totalMilk;
+            var totalAmount= list.Sum(e => e.Amount); 
+            ViewData["TotalAmount"] = totalAmount;
+            ViewData["StartDate"] = bill.StartDate;
+            ViewData["EndDate"] = bill.EndDate;
+            ViewData["IsPaid"] = bill.IsPaid;
+            return View();
         }
     }
 }
